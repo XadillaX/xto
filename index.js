6c5eadbc7614967452fb2c6c16cd2815b6183269
XadillaX <admin@xcoder.in>
2013-09-11 01:00:35 +0800

    some bug and details fixed

--- index.js
+++ index.js
@@ -19,13 +19,21 @@ exports.getCompanyInfo = function(company) {
     return null;
 }
 
+exports.isNumberValid = function(num, companyObject) {
+    if(companyObject["freg"] !== undefined) {
+        var reg = new RegExp(companyObject["freg"]);
+        return reg.test(num);
+    }
+}
+
 exports.query = function(num, company, callback) {
-    //company = company.toLowerCase();
+    var company_object = null;
     var exists = false;
     for(var i = 0; i < this.mapper.length; i++) {
         if(company === this.mapper[i]["companyname"] || company === this.mapper[i]["shortname"] ||
             company === this.mapper[i]["code"])
         {
+            company_object = this.mapper[i];
             exists = true;
             break;
         }
@@ -33,6 +41,13 @@ exports.query = function(num, company, callback) {
     if(!exists) {
         if(callback !== undefined) {
             callback.bind(this)(false, "This company is not supported yet.", {});
+        }
+        return;
+    }
+
+    if(!this.isNumberValid(num, company_object)) {
+        if(callback !== undefined) {
+            callback.bind(this)(false, "Invalid number.", {});
         }
         return;
     }
@@ -60,11 +75,15 @@ exports.query = function(num, company, callback) {
 };
 
 exports.stateToText = function(state) {
+    state = parseInt(state);
+    var value = "未知";
     switch(state) {
-        case 0 : return "在途中";
-        case 1 : return "已发货";
-        case 2 : return "疑难件";
-        case 3 : return "已签收";
-        case 4 : return "已退货";
+        case 0 : value = "在途中"; break;
+        case 1 : value = "已发货"; break;
+        case 2 : value = "疑难件"; break;
+        case 3 : value = "已签收"; break;
+        case 4 : value = "已退货"; break;
     }
+
+    return value;
 }
